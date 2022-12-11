2bbcd7667b62de2d1aa35026100a14ab543a3218
Enrico <enrico.manfron@ciandt.com>
2022-12-11 22:11:05 +0000

    [ST1] paralelize work threads

--- pratical_work_1/ac-nbody-resources/calc_chunk.c
+++ pratical_work_1/ac-nbody-resources/calc_chunk.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 
-#define N_BODIES 300
-#define N_THREADS 4
+#define N_BODIES 4
+#define N_THREADS 8
+
 #define WORK_AMOUNT (N_BODIES*(N_BODIES - 1)/(2))
 #define CHUNK_SIZE (WORK_AMOUNT/( N_THREADS))
 
@@ -15,8 +16,13 @@ int get_X(int i, int ntread){
     int x = (b - sqrt(b * b - 8 * CHUNK_SIZE )) / 2 ;
     
     printf("-----------------------------------------\nX: %d\n", x);
-    
-    return ntread == N_THREADS - 1 ? N_BODIES - 1 : x + i ;
+    x += (x == 0);
+    if ((ntread == N_THREADS - 1) || (i + x > N_BODIES - 1) || (x < 0)){
+        // printf("ERROR\n");
+        return N_BODIES - 1;
+    }
+    // printf("Noerror returning %d, %d\n", x + i, (int) x);
+    return x + i ;
 }
 
 
@@ -32,12 +38,11 @@ int tread(int i0, int i1, int tid){
 int main(){
     printf("WORK_AMOUNT: %d\n", WORK_AMOUNT);
     printf("CHUNK_SIZE: %d\n", CHUNK_SIZE);
-    int i = 0, i1, total = 0, tid = 0;
-    do{
+    int total = 0;
+    for (int tid = 0, i = 0, i1; tid < N_THREADS; tid++, i = i1){
         i1 = get_X(i, tid);
-        total += tread(i, i1, tid++);
-        i = i1;
-    }while(i1 < N_BODIES - 2);
+        total += tread(i, i1, tid);
+    }
     printf("-----------------------------------------\nTotal: %d\n", total);
     return 0;
 }
--- pratical_work_1/ac-nbody-resources/nbody-threads.c
+++ pratical_work_1/ac-nbody-resources/nbody-threads.c
@@ -1,11 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <pthread.h>
 
 #define CONST_GravConstant 0.01
+#define NUM_THREADS 8
  
 typedef struct{
-	double x,y,z;
+	double x, y, z;
 }vector;
  
 int GLOBAL_numBodies=15; // default number of bodies
@@ -18,116 +20,147 @@ double *GLOBAL_masses;
 vector *GLOBAL_positions;
 vector *GLOBAL_velocities;
 vector *GLOBAL_accelerations;
+vector **GLOBAL_partial_accelerations;
 
 //////////////////////////////////////////////////////////////////////// 
-vector addVectors(vector a,vector b){
-	vector c = {a.x+b.x,a.y+b.y,a.z+b.z};
- 
-	return c;
+typedef struct{
+	int start;
+	int end;
+}range;
+
+pthread_t threads[NUM_THREADS];
+range slices[NUM_THREADS];
+
+// create barrier to global aceleration
+pthread_barrier_t GLOBAL_barrier;
+
+int GLOBAL_WORK_PER_THREAD = 0;
+
+void updateWorkPerThread(void){
+	int work_amount = ((GLOBAL_numBodies*(GLOBAL_numBodies - 1))/2);
+	GLOBAL_WORK_PER_THREAD = work_amount/(NUM_THREADS);
+	printf("Work amount: %d, Work per thread: %d\n", work_amount, GLOBAL_WORK_PER_THREAD);
 }
 
-//////////////////////////////////////////////////////////////////////// 
-vector scaleVector(double b,vector a){
-	vector c = {b*a.x,b*a.y,b*a.z};
- 
-	return c;
+int sliceSize(int start){
+	int b = 2*(GLOBAL_numBodies - start) - 1;
+    int range = (b - sqrt(b * b - 8 * GLOBAL_WORK_PER_THREAD )) / 2;
+	int result = start + range + (range == 0);
+	if ((result > GLOBAL_numBodies - 1) || (range < 0))
+		return GLOBAL_numBodies - 1;
+	return result;
 }
- 
+
+void calculateSlices(void){
+	for(int i = 0, start = 0; i < NUM_THREADS; i++){
+		slices[i].start = start;
+		slices[i].end = (start = sliceSize(start));
+	}
+	slices[NUM_THREADS - 1].end = GLOBAL_numBodies - 1;	
+	for(int i = 0; i < NUM_THREADS; i++)
+		printf("Slice %d: %d - %d\n", i, slices[i].start, slices[i].end);
+}
+//////////////////////////////////////////////////////////////////////// 
+vector addVectors(vector a, vector b){
+	return (vector){a.x + b.x, a.y + b.y, a.z + b.z};
+}
+//////////////////////////////////////////////////////////////////////// 
+vector invertVector(vector a){
+	return (vector){-a.x, -a.y, -a.z};
+}
+//////////////////////////////////////////////////////////////////////// 
+vector scaleVector(double b, vector a){
+	return (vector){b * a.x, b * a.y, b * a.z};
+}
 ////////////////////////////////////////////////////////////////////////
 vector subtractVectors(vector a,vector b){
-	vector c = {a.x-b.x,a.y-b.y,a.z-b.z};
- 
-	return c;
+	return (vector){a.x - b.x, a.y - b.y, a.z - b.z};
 }
-
 ////////////////////////////////////////////////////////////////////////
 double mod(vector a){
-	return sqrt(a.x*a.x + a.y*a.y + a.z*a.z);
+	return sqrt(a.x * a.x + a.y * a.y + a.z * a.z);
 }
-
 ////////////////////////////////////////////////////////////////////////
 void initSystemFromRandom(){
-	int i;		
-
 	GLOBAL_masses = (double*)malloc(GLOBAL_numBodies*sizeof(double));
 	GLOBAL_positions = (vector*)malloc(GLOBAL_numBodies*sizeof(vector));
 	GLOBAL_velocities = (vector*)malloc(GLOBAL_numBodies*sizeof(vector));
 	GLOBAL_accelerations = (vector*)malloc(GLOBAL_numBodies*sizeof(vector));
- 
-	for(i=0;i<GLOBAL_numBodies;i++){
-		GLOBAL_masses[i]=rand()%GLOBAL_numBodies;
-		GLOBAL_positions[i].x=rand()%GLOBAL_windowWidth;
-		GLOBAL_positions[i].y=rand()%GLOBAL_windowHeight;
-		GLOBAL_positions[i].z=0;
-		GLOBAL_velocities[i].x=GLOBAL_velocities[i].y=GLOBAL_velocities[i].z=0;
+	GLOBAL_partial_accelerations = (vector**)malloc(GLOBAL_numBodies*sizeof(vector*));
+	
+	for(int i = 0; i < GLOBAL_numBodies; i++){
+		GLOBAL_masses[i] = rand() % GLOBAL_numBodies;
+		GLOBAL_positions[i] = (vector){rand() % GLOBAL_windowWidth, rand() % GLOBAL_windowHeight, 0};
+		GLOBAL_velocities[i] = (vector){0, 0, 0};
 	}
 }
-
+////////////////////////////////////////////////////////////////////////
+void initSystemMatrix(int start, int end){
+	for(int i = start; i < end; i++)
+		GLOBAL_partial_accelerations[i] = (vector*)malloc(GLOBAL_numBodies*sizeof(vector));
+}
 ////////////////////////////////////////////////////////////////////////
 void showSystem(){
-	int i;
-
-	for(i=0;i<GLOBAL_numBodies;i++) {
-		fprintf(stderr,"Body %d : %lf\t%lf\t%lf\t|\t%lf\t%lf\t%lf\n",i,\
-		GLOBAL_positions[i].x,GLOBAL_positions[i].y,GLOBAL_positions[i].z,\
-		GLOBAL_velocities[i].x,GLOBAL_velocities[i].y,GLOBAL_velocities[i].z);
+	for(int i = 0; i < GLOBAL_numBodies; i++) {
+		printf("Body %d : %lf\t%lf\t%lf\t|\t%lf\t%lf\t%lf\n",i,
+		GLOBAL_positions[i].x, GLOBAL_positions[i].y, GLOBAL_positions[i].z,
+		GLOBAL_velocities[i].x, GLOBAL_velocities[i].y, GLOBAL_velocities[i].z);
 	}
 }
 
 ////////////////////////////////////////////////////////////////////////
-void validateSystem(){
-	int i;
-	
-	for(i=0;i<GLOBAL_numBodies;i++) {
-		if (isnan(GLOBAL_positions[i].x) || isnan(GLOBAL_positions[i].y) || isnan(GLOBAL_positions[i].z) || \
+void validateSystem(int start, int end){
+	for(int i = start; i < end; i++) {
+		if (isnan(GLOBAL_positions[i].x) || isnan(GLOBAL_positions[i].y) || isnan(GLOBAL_positions[i].z) || 
 		    isnan(GLOBAL_velocities[i].x) || isnan(GLOBAL_velocities[i].y) || isnan(GLOBAL_velocities[i].z) ) {
-			fprintf(stderr,"NAN Body %d : %lf\t%lf\t%lf\t|\t%lf\t%lf\t%lf\n",i,\
+			printf("NAN Body %d : %lf\t%lf\t%lf\t|\t%lf\t%lf\t%lf\n",i,\
 			GLOBAL_positions[i].x,GLOBAL_positions[i].y,GLOBAL_positions[i].z,\
 			GLOBAL_velocities[i].x,GLOBAL_velocities[i].y,GLOBAL_velocities[i].z);
 			exit(1);
 		}
 	}
 }
-
 //////////////////////////////////////////////////////////////////////// 
-void computeAccelerations(){
-	int i,j;
- 
-	for(i=0;i<GLOBAL_numBodies;i++){
-		GLOBAL_accelerations[i].x = 0;
-		GLOBAL_accelerations[i].y = 0;
-		GLOBAL_accelerations[i].z = 0;
-		for(j=0;j<GLOBAL_numBodies;j++){
-			if(i!=j){
-				GLOBAL_accelerations[i] = addVectors(GLOBAL_accelerations[i],scaleVector(CONST_GravConstant*GLOBAL_masses[j]/pow(mod(subtractVectors(GLOBAL_positions[i],GLOBAL_positions[j])),3),subtractVectors(GLOBAL_positions[j],GLOBAL_positions[i])));
-			}
+void computePartialAccelerations(int start, int end){
+	for(int i = start; i < end; i++)
+		for(int j = i + 1; j < GLOBAL_numBodies; j++){
+			vector distance = subtractVectors(GLOBAL_positions[j], GLOBAL_positions[i]);
+			double distance_mod3 = pow(mod(distance), 3);
+			
+			vector acceleration_i = scaleVector(( CONST_GravConstant * GLOBAL_masses[j]) / distance_mod3, distance);
+			vector acceleration_j = scaleVector((-CONST_GravConstant * GLOBAL_masses[i]) / distance_mod3, distance);
+			
+			GLOBAL_partial_accelerations[i][j] = acceleration_i;
+			GLOBAL_partial_accelerations[j][i] = acceleration_j;
 		}
+}
+//////////////////////////////////////////////////////////////////////// 
+void computeAccelerations(int start, int end){
+	for(int i = start; i < end; i++){
+		GLOBAL_accelerations[i] = (vector){0, 0, 0};
+		for(int j = 0; j < GLOBAL_numBodies; j++)
+			if(i != j)
+				GLOBAL_accelerations[i] = addVectors(GLOBAL_accelerations[i], GLOBAL_partial_accelerations[i][j]);			
 	}
 }
-
 //////////////////////////////////////////////////////////////////////// 
-void computeVelocities(){
-	int i;
-
-	for(i=0;i<GLOBAL_numBodies;i++)
-		GLOBAL_velocities[i] = addVectors(GLOBAL_velocities[i],GLOBAL_accelerations[i]);
+void computeVelocities(int start, int end){
+	for(int i = start; i < end; i++)
+		GLOBAL_velocities[i] = addVectors(GLOBAL_velocities[i], GLOBAL_accelerations[i]);
 }
 
 //////////////////////////////////////////////////////////////////////// 
-void computePositions(){
-	int i;
-
-	for(i=0;i<GLOBAL_numBodies;i++)
-		GLOBAL_positions[i] = addVectors(GLOBAL_positions[i],addVectors(GLOBAL_velocities[i],scaleVector(0.5,GLOBAL_accelerations[i])));
+void computePositions(int start, int end){
+	for(int i = start; i < end; i++)
+		GLOBAL_positions[i] = addVectors(GLOBAL_positions[i],
+			addVectors(GLOBAL_velocities[i], scaleVector(0.5, GLOBAL_accelerations[i])));
 }
 
 ////////////////////////////////////////////////////////////////////////
-void resolveCollisions(){
-	int i,j;
-
-	for(i=0;i<GLOBAL_numBodies-1;i++)
-		for(j=i+1;j<GLOBAL_numBodies;j++){
-			if(GLOBAL_positions[i].x==GLOBAL_positions[j].x && GLOBAL_positions[i].y==GLOBAL_positions[j].y && GLOBAL_positions[i].z==GLOBAL_positions[j].z){
+void resolveCollisions(int start, int end){
+	for(int i = start; i < end; i++)
+		for(int j = i + 1; j < GLOBAL_numBodies; j++){
+			if(GLOBAL_positions[i].x == GLOBAL_positions[j].x && GLOBAL_positions[i].y == GLOBAL_positions[j].y && GLOBAL_positions[i].z ==GLOBAL_positions[j].z){
 				vector temp = GLOBAL_velocities[i];
 				GLOBAL_velocities[i] = GLOBAL_velocities[j];
 				GLOBAL_velocities[j] = temp;
@@ -136,18 +169,43 @@ void resolveCollisions(){
 }
 
 //////////////////////////////////////////////////////////////////////// 
-void simulate(){
-	computeAccelerations();
-	computePositions();
-	computeVelocities();
-	resolveCollisions();
-	validateSystem();
+void* simulate(void* arg){
+	long tid = (long)arg;
+	
+	int start_range = slices[tid].start; 
+	int end_range = slices[tid].end;
+
+	int work = GLOBAL_numBodies/NUM_THREADS, start_normal = -1, end_normal = -1;
+	if (work){
+		start_normal = tid*(work);
+		end_normal = tid == NUM_THREADS - 1? GLOBAL_numBodies : (start_normal + work);
+	}
+	else if(tid < GLOBAL_numBodies){
+		start_normal = tid;
+		end_normal = tid + 1;
+	}	
+	printf("Thread %ld : %d - %d\n", tid, start_normal, end_normal);
+	initSystemMatrix(start_normal, end_normal);
+	pthread_barrier_wait(&GLOBAL_barrier);
+	for(int i = 0; i < GLOBAL_numSteps; i++){
+		computePartialAccelerations(start_range, end_range);
+
+		pthread_barrier_wait(&GLOBAL_barrier);
+		computeAccelerations(start_normal, end_normal);
+		computePositions(start_normal, end_normal);
+		computeVelocities(start_normal, end_normal);
+
+		pthread_barrier_wait(&GLOBAL_barrier);
+		resolveCollisions(start_range, end_range);
+		pthread_barrier_wait(&GLOBAL_barrier);
+		validateSystem(start_normal, end_normal);
+		pthread_barrier_wait(&GLOBAL_barrier);
+	}
+	pthread_exit(NULL);
 }
 
 ////////////////////////////////////////////////////////////////////////
-int main(int argC,char* argV[])
-{
-	int i;
+int main(int argC,char* argV[]){
 	const unsigned int randSeed=10; // default value; do not change
 				
 	if (argC>=3) {		
@@ -167,19 +225,30 @@ int main(int argC,char* argV[])
 	srand(randSeed);
 	initSystemFromRandom();
 	//showSystem();
-
-	for(i=0;i<GLOBAL_numSteps;i++){
-				
-		//printf("%d\n", i);			
-		
-		simulate();	
-		//showSystem();		
-
+	updateWorkPerThread();
+	calculateSlices();
+	
+	int ret = pthread_barrier_init(&GLOBAL_barrier, NULL, NUM_THREADS);
+    if (ret){
+        printf("ERROR; return code from pthread_barrier_init() is %d\n", ret);
+        exit(ret);
+    }
+	
+	for(long tid = 0; tid < NUM_THREADS; tid++){
+		int ret = pthread_create(&threads[tid], NULL, simulate, (void*)tid);
+		if(ret){
+			printf("ERROR; return code from pthread_create() is %d\n", ret);
+			exit(ret);
+		}
 	}
-
-	// dump final result
+	for(long tid = 0; tid < NUM_THREADS; tid++){
+		int ret = pthread_join(threads[tid], NULL);
+		if(ret){
+			printf("ERROR; return code from pthread_join() is %d\n", ret);
+			exit(ret);
+		}
+	}				
 	showSystem();
-		
 	return 0;
 }
  
